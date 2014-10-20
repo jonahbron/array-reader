2e25fd81d03784de7fd568bc225bc61d99db29f0
Jonah Dahlquist <jonah@nucleussystems.com>
2014-10-20 16:56:32 -0700

    Added basics of array accessing methods

---
+++ src/ArrayReader.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace ArrayReader
+
+class ArrayReader {
+
+    private $data = null;
+    private $is_undefined = null;
+
+    public function __construct($data, $is_undefined=false) {
+        $this->data = $data;
+        $this->is_undefined = $is_undefined;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function get() {
+        $data = $this->data;
+        $is_undefined = false;
+        foreach (func_get_args() as $key) {
+            if (is_array($data)) {
+                if (array_key_exists($data, $key)) {
+                    $data = $data[$key];
+                } else {
+                    $data = array();
+                    $is_undefined = true;
+                    break;
+                }
+            } else {
+                $is_undefined = true;
+                break;
+            }
+        }
+        return new ArrayReader($data, $is_undefined);
+    }
+
+    public function has() {
+        $data = $this->data;
+        foreach (func_get_args() as $key) {
+            if (is_array($data)) {
+                if (array_key_exists($data, $key)) {
+                    $data = $data[$key];
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function value($default=null) {
+        if ($this->is_undefined) {
+            return $default;
+        } else {
+            return $this->data;
+        }
+    }
+
+    public function string($default='') {
+        return strval($this->value($default));
+    }
+
+    public function integer($default=0) {
+        return intval($this->value($default));
+    }
+
+    public function float($default=0.0) {
+        return doubleval($this->value($default));
+    }
+
+    public function array($default=array()) {
+        $value = $this->value($default);
+        if (is_array($value)) {
+            return $value;
+        } else {
+            return $default;
+        }
+    }
+
+    public function each($callback) {
+        foreach ($this->array() as $key => $value) {
+            $callback(new ArrayReader($value), $key);
+        }
+    }
+
+    public function do($callback) {
+        if (!$this->is_undefined) {
+            $callback($this);
+        }
+    }
+
+    private static function toArray($value) {
+        if (!is_array($value)) {
+            $value = array($value);
+        }
+        return $value;
+    }
+
+}
